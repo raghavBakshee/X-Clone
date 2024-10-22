0ceb92104e67b715c04278409707d036fc5626b0
raghavBakshee <raghav.bakshee@hitachids.com>
2024-10-22 18:41:53 +0530

    Prod Ready

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,16 @@ import cookieParser from "cookie-parser"
 import userRoute from "./routes/userRoute.js"
 import tweetRoute from "./routes/tweetRoute.js"
 import cors from 'cors'
+import path from "path"
 dotenv.config({
     path: ".env"
 })
 const PORT_ENV = process.env.PORT || 3001;
 dbConnection();
 const app = express();
+
+const _dirname = path.resolve();
+
 
 // middlewares
 app.use(express.urlencoded({
@@ -28,6 +32,10 @@ app.use(cors(corsOptions))
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/tweet", tweetRoute);
 
+app.use(express.static(path.join(_dirname, "/frontend/twitterclone/build")))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "twitterclone", "build", "index.html"))
+});
 app.listen(PORT_ENV, () => {
-   
+   console.log(`Server listens at port ${PORT_ENV} `)
 })
